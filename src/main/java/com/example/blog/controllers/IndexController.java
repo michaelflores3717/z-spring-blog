116322a9ef369491f25d8e39d92700ba03eedf3e
Mike Flores <mike.flores3717@gmail.com>
2019-05-01 14:03:13 -0500

    test

---
+++ src/main/java/com/example/blog/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String indexMethod(Model model) {
+        return "watsup?";
+    }
+}
